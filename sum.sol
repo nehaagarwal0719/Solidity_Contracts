26e8de41ce049b439580bdd367ca25a18c798310
Neha Agarwal <nehaagarwal0464@gmail.com>
2019-07-09 12:56:52 +0530

    Day1

---
+++ sum.sol
@@ -0,0 +1,14 @@
+pragma solidity 0.5.1;
+contract calculate{
+    
+    uint256  sum;
+    
+    event sum_cal (uint256 _num1,uint256 _num2);
+    
+    function cal(uint256 _num1, uint256 _num2 ) public returns (uint256) {
+        sum=_num1+_num2;
+         return sum;
+         emit sum_cal(2,3);
+    }
+    
+}
