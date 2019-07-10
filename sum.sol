0f7d198a044aec1363905e13a2345b7309f521c0
Neha Agarwal <nehaagarwal0464@gmail.com>
2019-07-10 12:27:24 +0530

    Update sum.sol

--- sum.sol
+++ sum.sol
@@ -1,10 +1,16 @@
 pragma solidity 0.5.1;
 contract calculate{
-
-    uint256  sum;
     
-    function cal(uint256 _num1, uint256 _num2 ) public returns (uint256) {
-        sum=_num1+_num2;
+    uint256  num1;
+    uint256  num2;
+
+    constructor(uint _num1,uint _num2)public{
+        num1=_num1;
+        num2=_num2;
+    }
+    
+    function cal( ) public view returns (uint) {
+         uint sum= num1 + num2;
          return sum;
-    }    
+    }   
 }
